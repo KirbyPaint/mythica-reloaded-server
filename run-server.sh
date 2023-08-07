5417ef98e38db0c4f12ef1ff17b1e85008017ab2
KirbyPaint <porter.ashley13@gmail.com>
2023-08-07 08:39:37 -0700

    update

--- run-server.sh
+++ run-server.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-java -Xms49152M -Xmx49152M -jar forge-1.16.5-36.2.39.jar nogui
+java -Xms49152M -Xmx49152M -jar /Users/kirbypaint/Desktop/mythica-reloaded-server/forge-1.16.5-36.2.39.jar nogui
